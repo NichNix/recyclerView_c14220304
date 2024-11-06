80f760f5f3c9c3b6215915669bc98410ce4e4c86
LAPTOP-B9KJSTFI\LENOVO <nicholasanthonyprasetyo@gmail.com>
2024-11-06 20:31:56 +0700

    Initial commit

---
+++ app/src/main/java/lat/paba/recyclerview/AdapterRecView.kt
@@ -0,0 +1,6 @@
+package lat.paba.recyclerview
+
+import androidx.recyclerview.widget.RecyclerView
+
+class AdapterRecView (private val listwayang: ArrayList<wayang>):RecyclerView.Adapter<AdapterRecView.ListViewHolder>{
+}
---
+++ app/src/main/java/lat/paba/recyclerview/wayang.kt
@@ -0,0 +1,8 @@
+package lat.paba.recyclerview
+
+data class wayang(
+    var foto : String,
+    var nama : String,
+    var karakter : String,
+    var deskripsi : String
+)
