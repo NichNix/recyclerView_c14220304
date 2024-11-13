836d12eb9dba020ad3cf1a56ed5f305a7d5bf0a6
LAPTOP-B9KJSTFI\LENOVO <nicholasanthonyprasetyo@gmail.com>
2024-11-13 20:06:15 +0700

    error on rendering the picture

--- app/src/main/java/lat/paba/recyclerview/AdapterRecView.kt
+++
@@ -1,6 +0,0 @@
-package lat.paba.recyclerview
-
-import androidx.recyclerview.widget.RecyclerView
-
-class AdapterRecView (private val listwayang: ArrayList<wayang>):RecyclerView.Adapter<AdapterRecView.ListViewHolder>{
-}
